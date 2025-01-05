8d46c9d3cec9401e5fb0cfb51a84ef3f329c2a95
dtacho <daria.tkz@gmail.com>
2025-01-05 20:57:59 +0200

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,19 @@
 import { defineCollection, z } from 'astro:content';
 
 const projectsCollection = defineCollection({
-	schema: z.object({
-		inProgress: z.boolean(),
-		title: z.string(),
-		description: z.string(),
-		tags: z.array(z.string()),
-		link: z.string(),
-		img_alt: z.string().optional(),
-	}),
+  schema: z.object({
+    inProgress: z.boolean(), // Indicates if the project is currently in progress
+    title: z.string(), // Title of the project
+    description: z.string(), // Brief description of the project
+    tags: z.array(z.string()), // Array of tags for categorization
+    link: z.string().url(), // URL linking to the project
+    image: z.object({
+      src: z.string(), // Path to the image file
+      alt: z.string().optional(), // Optional alt text for the image
+    }), // Object to define image properties
+  }),
 });
 
 export const collections = {
-	projects: projectsCollection,
+  projects: projectsCollection,
 };

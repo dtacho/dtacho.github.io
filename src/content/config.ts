0a0585c00339be49f52270d34594401540bed5f1
jackycao@outlook.com <jackycao@outlook.com>
2025-01-05 22:28:10 +0200

    wip: testing path changes - GitHub links the images differently to my local device, pushing to test

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,13 @@
 import { defineCollection, z } from 'astro:content';
 
 const projectsCollection = defineCollection({
-	schema: z.object({
+	schema: ({ image }) => z.object({
 		inProgress: z.boolean(),
 		title: z.string(),
 		description: z.string(),
 		tags: z.array(z.string()),
 		link: z.string(),
-    img: z.string(),
+    img: image(),
 		img_alt: z.string().optional(),
 	}),
 });
